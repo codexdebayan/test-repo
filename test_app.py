abfe585685a84d925bc7c03bfdc4aa5d60a3bab8
Grokr-gyanendra <gyanendra.shukla@datagrokr.com>
2024-11-14 15:54:18 +0530

    Update test file

--- test_app.py
+++ test_app.py
@@ -6,8 +6,9 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_add_user(client):
-    res = client.post('/users', query_string={'user':'Ram'})
-    assert res.status == 200
-    res = res.get_json()
-    assert res['msg'] == "User Inserted Successfully!"
+def test_fetch_user(client):
+    res = client.get('/users')
+    assert res.status_code == 200
+    res_json = res.get_json()
+    expected_users = ["Gyan", "Prince", "Shyam", "John"]
+    assert res_json == expected_users
